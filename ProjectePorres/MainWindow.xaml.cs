ca237231f853cda3542ad664fc615a3efbebe128
caicesar09 <caicesar09@gmail.com>
2023-05-13 20:51:05 +0200

    Fet els formularis de registre i inici de sessió

--- ProjectePorres/MainWindow.xaml.cs
+++ ProjectePorres/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MaterialDesignExtensions.Controls;
 using MaterialDesignExtensions.Model;
+using Ookii.Dialogs.Wpf;
 
 namespace ProjectePorres
 {
@@ -28,9 +29,61 @@ namespace ProjectePorres
             InitializeComponent();
         }
 
-        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        // Events
+        private void ButtonExit_Click(object sender, RoutedEventArgs e) => ShowExitDialog();
+
+
+        private void ItemHome_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            Application.Current.Shutdown();
+        }
+
+        private void ItemPorres_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void ItemEquips_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void ItemRecomanar_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void ItemExit_MouseUp(object sender, MouseButtonEventArgs e) => ShowExitDialog();
+
+        private void ItemSettings_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        // Methods
+
+        // Dialogs
+        private void ShowExitDialog()
+        {
+            using TaskDialog dialog = new();
+            dialog.WindowTitle = "Confirmació";
+            dialog.Content = "Vols sortir de l'aplicació o només tancar sessió";
+            dialog.MainIcon = TaskDialogIcon.Information;
+
+            TaskDialogButton buttonTancarSessio = new("Tancar sessió");
+            TaskDialogButton buttonTancarApp = new("Tancar app");
+            TaskDialogButton cancelButton = new(ButtonType.Cancel);
+
+            dialog.Buttons.Add(buttonTancarSessio);
+            dialog.Buttons.Add(buttonTancarApp);
+            dialog.Buttons.Add(cancelButton);
+
+            TaskDialogButton button = dialog.ShowDialog(this);
+
+            if (button == buttonTancarSessio)
+            {
+                Close();
+                // Obrir dialog d'inici de sessió.
+            }
+            else if (button == buttonTancarApp)
+            {
+                Application.Current.Shutdown();
+            }
         }
     }
 }
