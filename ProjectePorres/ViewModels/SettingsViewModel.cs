ce39ad48f541b788e5ff5b3befe22abe7124fbe0
caicesar09 <caicesar09@gmail.com>
2023-05-22 09:41:39 +0200

    Ara pots personalitzar els temes de l'aplicació a través del tauler de configuració.

--- ProjectePorres/ViewModels/SettingsViewModel.cs
+++ ProjectePorres/ViewModels/SettingsViewModel.cs
@@ -1,17 +1,108 @@
-﻿using System;
+﻿using MaterialDesignColors;
+using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ProjectePorres.ViewModels
 {
     class SettingsViewModel : BaseViewModel
     {
+        // Camps
+        private Swatch _primarySwatch;
+        private Swatch _accentSwatch;
+        private bool _darkThemeEnabled;
+
+        // Propietats
+        public IEnumerable<Swatch> Mostres { get; }
+
+        public Swatch PrimarySwatch 
+        {
+            get => _primarySwatch;
+            set
+            {
+                _primarySwatch = value;
+                OnPropertyChanged(nameof(PrimarySwatch));
+            }
+        }
+
+        public Swatch AccentSwatch
+        {
+            get => _accentSwatch;
+            set
+            {
+                _accentSwatch = value;
+                OnPropertyChanged(nameof(AccentSwatch));
+            }
+        }
+
+        public bool DarkThemeEnabled
+        {
+            get => _darkThemeEnabled;
+            set
+            {
+                _darkThemeEnabled = value;
+                OnPropertyChanged(nameof(DarkThemeEnabled));
+                AplicarDarkThemeCommand.Execute(this);
+            }
+        }
+
+        // Command
+        public ICommand AplicarTemaCommand { get; }
+        public ICommand AplicarAccentCommand { get; }
+        public ICommand AplicarDarkThemeCommand { get; }
 
         public SettingsViewModel()
         {
-            
+            Mostres = new SwatchesProvider().Swatches;
+            AplicarTemaCommand = new CommandViewModel(ExecuteAplicarTema);
+            AplicarAccentCommand = new CommandViewModel(ExecuteAplicarAccent);
+            AplicarDarkThemeCommand = new CommandViewModel(ExecuteAplicarDarkTheme);
+        }
+
+        private void ExecuteAplicarTema(object obj)  
+        {
+            if (PrimarySwatch != null)
+            {
+                ModificarTema(theme => theme.SetPrimaryColor(PrimarySwatch.ExemplarHue.Color));
+            }
+        } 
+
+        private void ExecuteAplicarAccent(object obj)
+        {
+            if (AccentSwatch is { AccentExemplarHue: not null })
+            {
+                ModificarTema(theme => theme.SetSecondaryColor(AccentSwatch.AccentExemplarHue.Color));
+            }
+        }
+
+        private void ExecuteAplicarDarkTheme(object obj)
+        {
+            var paletteHelper = new PaletteHelper();
+            ITheme theme = paletteHelper.GetTheme();
+
+            if (DarkThemeEnabled)
+            {
+                theme.SetBaseTheme(Theme.Dark);
+                paletteHelper.SetTheme(theme);
+            }
+            else
+            {
+                theme.SetBaseTheme(Theme.Light);
+                paletteHelper.SetTheme(theme);
+            }
+        }
+
+        private void ModificarTema(Action<ITheme> action)
+        {
+            var paletteHelper = new PaletteHelper();
+            ITheme theme = paletteHelper.GetTheme();
+
+            action?.Invoke(theme);
+
+            paletteHelper.SetTheme(theme);
         }
     }
 }
